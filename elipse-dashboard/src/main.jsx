07fe5914d9fc20ab270e8e02e85bbebbac587840
iviccpataro-eng <ivicc.pataro@gmail.com>
2025-09-12 21:14:21 -0300

    Update main.jsx

--- elipse-dashboard/src/main.jsx
+++ elipse-dashboard/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ElipseDashboard from "./App";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
