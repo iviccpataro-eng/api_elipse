30d0adcdb591f79e10f3ee5694cb4ee7ce0a999d
iviccpataro-eng <ivicc.pataro@gmail.com>
2025-09-18 11:48:07 -0300

    Update App.jsx (fix bug)

--- elipse-dashboard/src/App.jsx
+++ elipse-dashboard/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState, useMemo } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { RadialBarChart, RadialBar, PolarAngleAxis } from "recharts";
 
 import ToolsPage from "./ToolsPage";
