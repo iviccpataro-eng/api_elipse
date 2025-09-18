7cc69ea83953c051e6ac252381c1cd4cca631863
iviccpataro-eng <ivicc.pataro@gmail.com>
2025-09-18 11:51:31 -0300

    Update LoginPage.jsx (fix bug)

--- elipse-dashboard/src/LoginPage.jsx
+++ elipse-dashboard/src/LoginPage.jsx
@@ -1,7 +1,7 @@
 // LoginPage.jsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 export default function LoginPage() {
     const [user, setUser] = useState("");
