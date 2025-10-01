5d6beabc917286cf71a89ce48d8137ce207433e6
iviccpataro-eng <ivicc.pataro@gmail.com>
2025-10-01 09:53:25 -0300

    Update UpdateProfile.jsx

--- elipse-dashboard/src/ToolsPage.jsx
+++ elipse-dashboard/src/ToolsPage.jsx
@@ -45,6 +45,9 @@ export default function ToolsPage() {
                             }`}
                     >
                         Gerar Convite
+                        <span class="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2 z-10 whitespace-nowrap">
+                            Somente usuários com privilégios de administrador
+                        </span>
                     </button>
                 </nav>
             </aside>
