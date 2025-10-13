efce0335068189def7ba9b8e07715b57bbc4b8ae
iviccpataro-eng <ivicc.pataro@gmail.com>
2025-10-13 10:27:11 -0300

    .

--- projeto_bcrypt/compare.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require("bcrypt");
-const hash = "$2b$10$bD7P9sYXe.UKvHthg8u8kuMfX8i.Dpz8Ou7XQ2SXrAIQ6hQL9Pjlm";
-const senhaDigitada = "R@isenT3ch";
-
-bcrypt.compare(senhaDigitada, hash).then(console.log);
