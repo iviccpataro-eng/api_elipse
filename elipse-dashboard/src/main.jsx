61bd96710f48aca2f5cf32193dc689c8ff69b749
iviccpataro-eng <ivicc.pataro@gmail.com>
2025-10-24 15:33:18 -0300

    Update main.jsx

--- elipse-dashboard/src/main.jsx
+++ elipse-dashboard/src/main.jsx
@@ -1,8 +1,8 @@
+import "./ChartSetup";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./components/ChartSetup";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
